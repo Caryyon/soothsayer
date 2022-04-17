f5d885ea2fbafcaa83882545d98ed571d2505bc4
Cary Wolff <cwolff@seer.la>
2022-04-17 00:05:20 -0500

    feat: first command added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use std::process::{Command, Stdio};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -10,18 +11,23 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Runs the provided script
+    /// Runs the provided npm script
     Run { command: Option<String> },
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
     match &cli.command {
         Commands::Run { command } => {
-            println!("'ss run' was used with the given command: {:?}", command)
+
+        let output = Command::new("npm")
+            .arg(command.as_ref().unwrap())
+            .stdout(Stdio::inherit())
+            .output()
+            .expect("Failed to execute command");
+
+            assert_eq!(String::from_utf8_lossy(&output.stdout), "");
         }
     }
 }
