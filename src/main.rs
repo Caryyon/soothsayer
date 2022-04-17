0f4af193fa376c675de5900f6f4a4bf94525d439
Cary Wolff <cwolff@seer.la>
2022-04-16 22:57:34 -0500

    feat: adds first subcomman

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Runs the provided script
+    Run { command: Option<String> },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Run { command } => {
+            println!("'ss run' was used with the given command: {:?}", command)
+        }
+    }
+}
